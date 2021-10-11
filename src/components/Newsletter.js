0e78a1a85671c8c89bee8565adf1f2e7dcdac97d
bartoszgawronski <bartosz.gawronski@sts.pl>
2021-10-11 11:43:14 +0200

    feat: submit newlsetter message []

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -37,6 +37,23 @@ const Newsletter = () => {
         setValues({[e.target.name]: e.target.value});
     }
 
+    const form = submitted ? (<div className="min-w-max font-medium items-center flex"><p className="my-auto text-center bg-blue p-4 rounded-lg text-white"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-auto mb-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+    </svg><span>Dzięki za zapis. <br/> Odezwiemy się do Ciebie za chwilkę.</span></p></div>) : (
+        <div className="ml-8">
+            <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="newsletter-akademia" onSubmit={handleSubmit}>
+                <input name="bot-field" type="hidden" onChange={handleChange}/>
+                <div>
+                    <input name="name" id="name" required placeholder="Imie" type="text" onChange={handleChange} className="w-input my-2 rounded-lg h-9 px-2 py-3"/>
+                </div>
+                <div>
+                    <input name="email" id="email" required placeholder="Email" type="email" onChange={handleChange} className="w-input my-2 h-9 rounded-lg px-2 py-3"/>
+                </div>
+                <button className="w-input bg-blue my-2 h-9 rounded-lg text-white">Zapisz się</button>
+            </form>
+        </div>
+    )
+
     return (
         <div className="m-auto w-container p-8">
             <Box>
@@ -48,18 +65,7 @@ const Newsletter = () => {
                             Szkolenia mają charakter doskonalący. Część z nich jest prowadzona w trybie indywidualnym. Naukę możesz rozpocząć w dowolnym momencie i z tempem, jaki jest dla Ciebie najlepszy.
                         </p>
                     </div>
-                    <div className="ml-8">
-                        <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="newsletter-akademia" onSubmit={handleSubmit}>
-                            <input name="bot-field" type="hidden" onChange={handleChange}/>
-                            <div>
-                                <input name="name" id="name" required placeholder="Imie" type="text" onChange={handleChange} className="w-input my-2 rounded-lg h-9 px-2 py-3"/>
-                            </div>
-                            <div>
-                                <input name="email" id="email" required placeholder="Email" type="email" onChange={handleChange} className="w-input my-2 h-9 rounded-lg px-2 py-3"/>
-                            </div>
-                            <button className="w-input bg-blue my-2 h-9 rounded-lg text-white">Zapisz się</button>
-                        </form>
-                    </div>
+                    {form}
                 </div>
                 {submitted}
             </Box>
